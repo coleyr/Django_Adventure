3a817a9e5c403c274766b3a99bc5fe00b2e63f32
Coley Angel <coleyrangel@gmail.com>
2021-09-25 11:42:17 -0400

    Added hint model

--- Adventure/models.py
+++ Adventure/models.py
@@ -30,6 +30,7 @@ class Adventure(models.Model):
             models.UniqueConstraint(fields=['name'], name='adventure_name_constraint')
         ]
 
+
 class Audio(models.Model):
     audiofile = models.FileField(upload_to='audio')
     name = models.CharField(max_length=100)
@@ -53,7 +54,20 @@ class Video(models.Model):
             models.UniqueConstraint(fields=['videofile'], name='videofile_unique_constraint')
         ]
   
+class Hint(models.Model):
+    name = models.CharField(max_length=200, validators=[validate_slug])
+    audio = models.ForeignKey(Audio, on_delete=models.SET_NULL, related_query_name="audio", blank=True, null=True)
+    video = models.ForeignKey(Video, on_delete=models.SET_NULL, related_query_name="video", blank=True, null=True)
+    image = models.ForeignKey(Image, on_delete=models.SET_NULL, related_query_name="image", blank=True, null=True)
+    hint_message = models.TextField(max_length=500)
 
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['hint_message', 'name'], name='hint_name_constraint')
+        ]
 
 ClueOrder = models.IntegerChoices('ClueOrder', 'FIRST SECOND THIRD FOURTH FIFTH SIXTH SEVENTH EIGHTH NINTH TENTH ELEVENTH TWELFTH THIRTEENTH FOURTEENTH FIFTEENTH')
 
@@ -68,6 +82,7 @@ class Clue(models.Model):
     audio = models.ForeignKey(Audio, on_delete=models.SET_NULL, related_query_name="audio", blank=True, null=True)
     video = models.ForeignKey(Video, on_delete=models.SET_NULL, related_query_name="video", blank=True, null=True)
     image = models.ForeignKey(Image, on_delete=models.SET_NULL, related_query_name="image", blank=True, null=True)
+    hint = models.ManyToManyField(Hint, related_query_name="hint", blank=True, null=True)
     def __str__(self):
         return f'{self.adventure.name} - Clue{self.clueorder}'
 
@@ -75,4 +90,5 @@ class Clue(models.Model):
         constraints = [
             models.UniqueConstraint(fields=['adventure', 'clueorder'], name='clue_adventure_constraint')
         ]
+
 
--- Adventure/static/js/typing.js
+++ Adventure/static/js/typing.js
@@ -3,23 +3,26 @@ var caption = '';
 
 
 $(document).ready(function() {
-    setInterval ('cursorAnimation()', 600);
+    setInterval('cursorAnimation()', 600);
     captionEl = $('#caption');
     let pressed = false;
 
-    $('#begin').click(function(){
-        if (!pressed){
-        document.getElementById('begin').classList.add('hidden')
-        testTypingEffect();
-        play()
-        pressed = true}
+    $('#begin').click(function() {
+        if (!pressed) {
+            document.getElementById('begin').classList.add('hidden')
+            testTypingEffect();
+            // play()
+            pressed = true
+        }
     });
 
 });
+
 function play() {
     const audio = document.getElementById('Audio');
     audio.play();
-  }
+}
+
 function testTypingEffect() {
     caption = $('#user-caption').text();
     type();
@@ -27,23 +30,25 @@ function testTypingEffect() {
 
 function type() {
     captionEl.html(caption.substr(0, captionLength++));
-    if (captionLength == caption.length){
+    if (captionLength == caption.length) {
         setTimeout('type()', 75);
         showNext();
-    } else if (captionLength < caption.length+1){
+    } else if (captionLength < caption.length + 1) {
         setTimeout('type()', 75);
-    }   else {
+    } else {
         captionLength = 0;
         caption = '';
     }
 }
-function showNext(){
+
+function showNext() {
     document.getElementById('nextbutton').classList.remove('hidden')
 }
+
 function testErasingEffect() {
     caption = captionEl.html();
     captionLength = caption.length;
-    if (captionLength>0) {
+    if (captionLength > 0) {
         erase();
     } else {
         $('#caption').html("You didn't write anything to erase, but that's ok!");
@@ -53,12 +58,12 @@ function testErasingEffect() {
 
 function erase() {
     captionEl.html(caption.substr(0, captionLength--));
-    if(captionLength >= 0) {
+    if (captionLength >= 0) {
         setTimeout('erase()', 50);
     } else {
         captionLength = 0;
         caption = '';
-    }	
+    }
 }
 
 function cursorAnimation() {
