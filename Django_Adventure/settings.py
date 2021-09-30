e664555bd2a6ca81972cc9a0872c2e520bc0c9a0
Coley Angel -T (coangel - INSIGHT GLOBAL INC at Cisco) <coangel@cisco.com>
2021-09-30 11:43:41 -0400

    Added static files settings

--- Django_Adventure/settings.py
+++ Django_Adventure/settings.py
@@ -128,3 +128,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 MEDIA_URL='/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, "/var/www/cltadventure.com/static/")
